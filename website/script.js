1997add1f82c25f4f8a360e57896a9ded2d94666
KB <kwabena.baahboakye@gmail.com>
2025-12-06 15:50:57 -0500

    email fix

--- website/script.js
+++ website/script.js
@@ -1,6 +1,5 @@
 // Grace Found Home Care Agency - JavaScript
 
-// IMPORTANT: Update this URL after deploying your Cloudflare Worker
 const CONTACT_FORM_API = 'https://gracefound-contact-form.kwabena-baahboakye.workers.dev';
 
 document.addEventListener('DOMContentLoaded', function() {
